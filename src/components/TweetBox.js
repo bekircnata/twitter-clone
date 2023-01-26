5abf3af3d8fe4b396a0eb516376f3133b5e35a5a
Bekir Can Ata <bekircnata@gmail.com>
2023-01-26 18:37:41 +0300

    Kullanilmayan importlar kaldirildi

--- src/components/TweetBox.js
+++ src/components/TweetBox.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import "./TweetBox.scss"
 
 import { Button } from "primereact/button";
